a3762e304c9d392ddf5511d351c813359e32af97
Argenis De La Rosa <hennyrosexo@gmail.com>
2019-08-13 22:41:44 -0400

    Initial Commit

---
+++ Landmarks/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  Landmarks
+//
+//  Created by argenis delarosa on 8/13/19.
+//  Copyright © 2019 argenis delarosa. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+#if DEBUG
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+#endif
